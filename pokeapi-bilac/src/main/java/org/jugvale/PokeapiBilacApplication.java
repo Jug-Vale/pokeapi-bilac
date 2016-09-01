6448eafd6009aacfbb0039c16c4ba8640eec7d60
pedro-hos <pedro-hos@outlook.com>
2016-08-31 23:56:52 -0300

    first commit

---
+++ pokeapi-bilac/src/main/java/org/jugvale/PokeapiBilacApplication.java
@@ -0,0 +1,12 @@
+package org.jugvale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeapiBilacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeapiBilacApplication.class, args);
+	}
+}
